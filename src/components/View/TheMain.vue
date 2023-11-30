d58eb812dd7149d44c6f0704796a826ae7367534
Marcin Kosecki <marcin.kosecki@int.pl>
2023-11-30 20:32:45 +0100

    Remove keep-alive from router-view

--- src/components/View/TheMain.vue
+++ src/components/View/TheMain.vue
@@ -1,13 +1,9 @@
 <template>
     <main class="main">
-        <router-view v-slot="{Component}">
-            <keep-alive include="Product">
-                <component :is="Component" />
-            </keep-alive>
-        </router-view>
+        <router-view></router-view>
     </main>
 </template>
-<script lang="ts"></script>
+
 <style scoped>
 .main {
     margin-top: 8rem;
