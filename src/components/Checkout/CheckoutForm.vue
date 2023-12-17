ba6babd54870c29ad7a3caa818236d6b79a3a215
Marcin Kosecki <marcin.kosecki@int.pl>
2023-12-17 09:30:17 +0100

    Add CheckoutForm components

---
+++ src/components/Checkout/CheckoutForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <form v-on:submit.prevent="onSubmit">
+        <checkout-billing></checkout-billing>
+        <checkout-shipping></checkout-shipping>
+        <checkout-payment></checkout-payment>
+
+        <form-element
+            :element="'submit'"
+            :class="`checkout__submit`"
+            :classInput="`checkout__submit-btn`"
+            :type="'submit'"
+            :text="'submit'"
+        ></form-element>
+    </form>
+</template>
+
+<script setup lang="ts">
+import FormElement from '../Shared/FormElement.vue';
+import CheckoutBilling from '@/components/Checkout/CheckoutBilling.vue';
+import CheckoutShipping from '@/components/Checkout/CheckoutShipping.vue';
+import CheckoutPayment from '@/components/Checkout/CheckoutPayment.vue';
+
+const onSubmit = () => {
+    console.log('ss');
+};
+</script>
+
+<style scoped></style>
